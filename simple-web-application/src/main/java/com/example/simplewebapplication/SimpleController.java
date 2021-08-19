149b6cd0f0145c538bee8f04bb95bd54309f3059
yasuomaidana <yasuomaidana@gmail.com>
2021-08-19 14:56:32 -0500

    Simple application done

--- simple-web-application/src/main/java/com/example/simplewebapplication/SimpleController.java
+++ simple-web-application/src/main/java/com/example/simplewebapplication/SimpleController.java
@@ -1,4 +1,15 @@
 package com.example.simplewebapplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController //Ready for use Spring embassy to handle web request
 public class SimpleController {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello world";
+    }
+    @RequestMapping("/2")
+    public String index2(){
+        return "The idea works";
+    }
 }
--- simple-web-application/src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
+++ simple-web-application/src/main/java/com/example/simplewebapplication/SimpleWebApplication.java
@@ -1,7 +1,12 @@
 package com.example.simplewebapplication;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SimpleWebApplication {
@@ -10,4 +15,16 @@ public class SimpleWebApplication {
 		SpringApplication.run(SimpleWebApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		 return args -> {
+		 	System.out.println("Lets inspect the beans provided by Spring boot");
+		 	String[] beanNames = ctx.getBeanDefinitionNames();
+			 Arrays.sort(beanNames);
+			 for(String beanName:beanNames){
+			 	System.out.println(beanName);
+			 }
+		 };
+	}
 }
+//check functionality using >: curl localhost:8080
