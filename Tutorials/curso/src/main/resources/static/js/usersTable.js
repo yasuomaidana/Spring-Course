78e3d16e6bcff0cf9c3a67f81650aaeb829a9488
yasuomaidana <yasuomaidana@gmail.com>
2021-08-31 10:02:58 -0500

    modified to acces data from serverlet

--- Tutorials/curso/src/main/resources/static/js/usersTable.js
+++ Tutorials/curso/src/main/resources/static/js/usersTable.js
@@ -1,4 +1,18 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+    loadUsers();
+    $('#users').DataTable();
 });
+
+async function loadUsers(){//Since we used await we have to define this function as an asynchronic
+    const request = await fetch("user/123",{//await make request to wait the answer, all the program halts until the function is resolved
+        method: 'GET', 
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        }
+    });
+    const user = await request.json();
+    console.log(user);
+
+}
