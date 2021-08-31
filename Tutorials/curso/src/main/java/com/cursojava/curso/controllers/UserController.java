a28c3c4d30206a7989b0b1ed8f639fc61a5f26f3
yasuomaidana <yasuomaidana@gmail.com>
2021-08-31 10:02:25 -0500

    users path controller created

--- Tutorials/curso/src/main/java/com/cursojava/curso/controllers/UserController.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/controllers/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController//Indicates that it is a controller
 public class UserController {
     @RequestMapping(value = "proof")
@@ -31,5 +34,22 @@ public class UserController {
         user.setLastname("Dr");
         user.setPhone("1234567890");
         return user;
+    }
+    private User createUser(Long Id,String Name,String Lastname,String Phone){
+        User user = new User();
+        user.setId(Id);
+        user.setName(Name);
+        user.setLastname(Lastname);
+        user.setEmail(Name+"_"+Lastname+"@idc.idk");
+        user.setPhone(Phone);
+        return user;
+    }
+    @RequestMapping(value="users")
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        users.add(createUser(123L,"John","Black","1234"));
+        users.add(createUser(124L,"Mike","Wasabi","1235"));
+        users.add(createUser(125L,"Buzz","Lightyear","1236"));
+        return users;
     }
 }
