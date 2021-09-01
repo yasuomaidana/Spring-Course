571a21319931d222246096ecc54f7f408c38449a
yasuomaidana <yasuomaidana@gmail.com>
2021-09-01 18:17:08 -0500

    modified to consume data from serverlet

---
+++ Tutorials/curso/src/main/java/com/cursojava/curso/dao/UserDao.java
@@ -0,0 +1,2 @@
+package com.cursojava.curso.dao;public interface UserDao {
+}
--- Tutorials/curso/src/main/resources/static/js/usersTable.js
+++ Tutorials/curso/src/main/resources/static/js/usersTable.js
@@ -4,15 +4,24 @@ $(document).ready(function() {
     $('#users').DataTable();
 });
 
+convertToRow=(users)=>{
+    let row = "";
+    users.forEach(user=>{
+        let rowContent =`<td>${user.id}</td><td>${user.name} ${user.lastname}</td><td>${user.email}</td><td>${user.phone}</td>
+        <td><button class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></button></td>`
+        let tr = `<tr>${rowContent}</tr>`;
+        row+=tr;
+    });
+    return row;
+}
 async function loadUsers(){//Since we used await we have to define this function as an asynchronic
-    const request = await fetch("user/123",{//await make request to wait the answer, all the program halts until the function is resolved
+    const request = await fetch("users",{//await make request to wait the answer, all the program halts until the function is resolved
         method: 'GET', 
         headers:{
             'Accept':'application/json',
             'Content-Type':'application/json'
         }
     });
-    const user = await request.json();
-    console.log(user);
-
+    const users = await request.json();
+    document.querySelector('#users tbody').outerHTML = convertToRow(users);
 }
