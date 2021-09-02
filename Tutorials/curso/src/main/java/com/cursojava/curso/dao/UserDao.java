eb56052891526c3bb9e6d3cd3c99a09fb1364d87
yasuomaidana <yasuomaidana@gmail.com>
2021-09-02 15:48:30 -0500

    User Dao, interface and implementation

--- Tutorials/curso/src/main/java/com/cursojava/curso/dao/UserDao.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/dao/UserDao.java
@@ -1,4 +1,9 @@
 package com.cursojava.curso.dao;
 
+import com.cursojava.curso.models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> getUsers();
 }
--- Tutorials/curso/src/main/java/com/cursojava/curso/dao/UserDaoImp.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/dao/UserDaoImp.java
@@ -1,2 +1,24 @@
-package com.cursojava.curso.dao;public class UserDaoImp {
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository //Makes reference to the database connection,
+// it says that this class could have access to database
+@Transactional //Proportionate the capability of make SQL request
+public class UserDaoImp implements UserDao{
+    @PersistenceContext
+    private EntityManager entityManager;//Helps to make the connection to DB
+
+    @Override
+    @Transactional
+    public List<User> getUsers() {
+        String query ="FROM User";//Makes reference to our MODEL CLASS
+        return entityManager.createQuery(query).getResultList();//Hibernates queries are not SQL queries
+    }
 }
