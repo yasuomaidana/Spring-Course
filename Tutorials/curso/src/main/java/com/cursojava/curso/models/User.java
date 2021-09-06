a6d922516b4e27dc1c2d2f1b55a3d745bcc05c59
yasuomaidana <yasuomaidana@gmail.com>
2021-09-06 15:28:28 -0500

    lombok added to make setter and getter easier

--- Tutorials/curso/src/main/java/com/cursojava/curso/models/User.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/models/User.java
@@ -1,5 +1,8 @@
 package com.cursojava.curso.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,59 +13,17 @@ import javax.persistence.Table;
 public class User {
     //we could have mapped each property using @Column(name="column name")
     //however, given that the properties and column names matches, we don't need to do it
+    @Getter @Setter
     @Id
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private String lastname;
+    @Getter @Setter
     private String email;
+    @Getter @Setter
     private String phone;
+    @Getter @Setter
     private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
