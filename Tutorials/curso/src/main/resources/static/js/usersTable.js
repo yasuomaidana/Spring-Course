7a385dba79556416ea91b2778e6895f111cb108b
yasuomaidana <yasuomaidana@gmail.com>
2021-09-06 16:14:44 -0500

    Delete function added

--- Tutorials/curso/src/main/resources/static/js/usersTable.js
+++ Tutorials/curso/src/main/resources/static/js/usersTable.js
@@ -7,15 +7,16 @@ $(document).ready(function() {
 convertToRow=(users)=>{
     let row = "";
     users.forEach(user=>{
+        let button = `<button class="btn btn-danger btn-circle btn-sm" onclick="deleteUser(${user.id})"><i class="fas fa-trash"></i></button>`;
         let rowContent =`<td>${user.id}</td><td>${user.name} ${user.lastname}</td><td>${user.email}</td><td>${user.phone}</td>
-        <td><button class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></button></td>`
+        <td>${button}</td>`
         let tr = `<tr>${rowContent}</tr>`;
         row+=tr;
     });
     return row;
 }
 async function loadUsers(){//Since we used await we have to define this function as an asynchronic
-    const request = await fetch("users",{//await make request to wait the answer, all the program halts until the function is resolved
+    const request = await fetch("api/users",{//await make request to wait the answer, all the program halts until the function is resolved
         method: 'GET', 
         headers:{
             'Accept':'application/json',
@@ -24,4 +25,18 @@ async function loadUsers(){//Since we used await we have to define this function
     });
     const users = await request.json();
     document.querySelector('#users tbody').outerHTML = convertToRow(users);
+}
+
+async function deleteUser(id){
+    if(!confirm('Do you want to delete this user?')){
+        return;
+    }
+    const request = await fetch(`api/user/${id}`,{
+        method: 'DELETE', 
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        }
+    });
+    location.reload();
 }
