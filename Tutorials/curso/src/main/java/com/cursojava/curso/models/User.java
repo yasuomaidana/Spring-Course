2b5d8643539911ab4f7ff594f1d9d16710429257
yasuomaidana <yasuomaidana@gmail.com>
2021-09-08 19:58:25 -0500

    Modified to support autogenerate id

--- Tutorials/curso/src/main/java/com/cursojava/curso/models/User.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/models/User.java
@@ -2,11 +2,10 @@ package com.cursojava.curso.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.beans.factory.annotation.Value;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="users")//Makes reference to the table
@@ -15,6 +14,7 @@ public class User {
     //however, given that the properties and column names matches, we don't need to do it
     @Getter @Setter
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Used to auto fix the ID
     private Long id;
     @Getter @Setter
     private String name;
@@ -23,7 +23,7 @@ public class User {
     @Getter @Setter
     private String email;
     @Getter @Setter
-    private String phone;
+    private String phone; //It was set as default value null from database
     @Getter @Setter
     private String password;
 }
