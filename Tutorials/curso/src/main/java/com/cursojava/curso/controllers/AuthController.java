8b8e69dde44feef23fac3540713ec976be3d9fd1
yasuomaidana <yasuomaidana@gmail.com>
2021-09-11 07:38:26 -0500

    AuthController modified to get Token

--- Tutorials/curso/src/main/java/com/cursojava/curso/controllers/AuthController.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.dao.UserDao;
 import com.cursojava.curso.models.User;
+import com.cursojava.curso.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @RequestMapping(value = "api/login",method = RequestMethod.POST)
     public String login(@RequestBody User user){
-        String res;
-        if(userDao.verifyEmailPassword(user)) res="Oki";
-        else res="Bad guy, bad buy";
-        return res;
+        User verifiedUser = userDao.getVerifiedUser(user);
+        if(verifiedUser!=null) {
+            //returns token
+            return jwtUtil.createToken(String.valueOf(verifiedUser.getPassword()),verifiedUser.getEmail());
+        }
+        else return "FAIL";
     }
 }
