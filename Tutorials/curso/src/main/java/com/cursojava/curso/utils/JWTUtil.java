b9f051fd9fd538e7071d964f646e2db1363c3102
yasuomaidana <yasuomaidana@gmail.com>
2021-09-11 07:41:40 -0500

    JWT token generator created and implemented

--- Tutorials/curso/src/main/java/com/cursojava/curso/utils/JWTUtil.java
+++ Tutorials/curso/src/main/java/com/cursojava/curso/utils/JWTUtil.java
@@ -1,2 +1,67 @@
-package com.cursojava.curso.utils;public class JWTUtil {
+package com.cursojava.curso.utils;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+
+@Component /*It allows us to convert the class into a component,
+so it can be accessed through @Autowired and use @Value
+*/
+public class JWTUtil {
+    /*@Value("${variable}") this variable is stored in
+    * src/main/resources/application.properties*/
+    @Value("${security.jwt.secret}")
+    private String secret;
+
+    @Value("${security.jwt.issuer}")
+    private String issuer;
+
+    @Value("${security.jwt.ttlMillis}")
+    private long ttlMillis;
+
+    //Used to see errors
+    private final Logger log = LoggerFactory.getLogger(JWTUtil.class);
+
+    /**
+     * Create a new token.
+     *
+     * @param id
+     * @param subject
+     * @return
+     */
+    public String createToken(String id, String subject) {//creates the jwt
+
+        // The JWT signature algorithm used to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //  sign JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secret);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //  set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id).setIssuedAt(now).setSubject(subject).setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        // Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
 }
