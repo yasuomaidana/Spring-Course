84bc1e13a5073e880de8c99d4a239295e9e21121
yasuomaidana <yasuomaidana@gmail.com>
2021-09-21 12:18:54 -0500

    Reddit Clone project started

---
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/ReditCloneApplication.java
@@ -0,0 +1,13 @@
+package com.JavaSpringPractice.ReditClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReditCloneApplication.class, args);
+	}
+
+}
---
+++ Tutorials/ReditClone/src/test/java/com/JavaSpringPractice/ReditClone/ReditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpringPractice.ReditClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
