9a18a49ec4f5a41d5b3eee6e0e641e0a5abb0b78
yasuomaidana <yasuomaidana@gmail.com>
2021-09-22 19:24:16 -0500

    /signup path controller done

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/controller/AuthController.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.JavaSpringPractice.ReditClone.controller;
 
 import com.JavaSpringPractice.ReditClone.dto.RegisterRequest;
+import com.JavaSpringPractice.ReditClone.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
+    private final AuthService authService;
     @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest){
-
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User registration Successful", HttpStatus.OK);
     }
 }
