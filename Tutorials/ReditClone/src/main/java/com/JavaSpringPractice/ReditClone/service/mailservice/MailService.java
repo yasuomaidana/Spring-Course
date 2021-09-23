d3eb47f67ae49c18e337440c74a82ca30cebe6f1
yasuomaidana <yasuomaidana@gmail.com>
2021-09-22 19:35:57 -0500

    Mail service created
    Here we created a service that creates a thymeleaf response views

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/mailservice/MailService.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/mailservice/MailService.java
@@ -1,2 +1,34 @@
-package com.JavaSpringPractice.ReditClone.service.mailservice;public class MailService {
+package com.JavaSpringPractice.ReditClone.service.mailservice;
+
+import com.JavaSpringPractice.ReditClone.exceptions.SpringRedditException;
+import com.JavaSpringPractice.ReditClone.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j //LoggerField
+public class MailService {
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+    public void sendMail(NotificationEmail notificationEmail){
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@gmail.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
+        };
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation mail sent");
+        }catch (MailException e){
+            throw new SpringRedditException("Exception occurred when sending email to :"+ notificationEmail.getRecipient());
+        }
+    }
 }
