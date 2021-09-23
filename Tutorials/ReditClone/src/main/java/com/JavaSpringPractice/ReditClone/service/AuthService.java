6f249c2ebeacc52e6c448f857c1a70954346d6e7
yasuomaidana <yasuomaidana@gmail.com>
2021-09-22 19:30:12 -0500

    Authorization service created
    We used thymeleaf to generate a view, additionally this service uses encription to protect the password

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/AuthService.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/AuthService.java
@@ -1,9 +1,14 @@
 package com.JavaSpringPractice.ReditClone.service;
 
 import com.JavaSpringPractice.ReditClone.dto.RegisterRequest;
+import com.JavaSpringPractice.ReditClone.model.NotificationEmail;
 import com.JavaSpringPractice.ReditClone.model.User;
+import com.JavaSpringPractice.ReditClone.model.token.VerificationToken;
 import com.JavaSpringPractice.ReditClone.repository.UserRepository;
+import com.JavaSpringPractice.ReditClone.repository.VerificationTokenRepository;
+import com.JavaSpringPractice.ReditClone.service.mailservice.MailService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +22,8 @@ public class AuthService {
 
      private final PasswordEncoder passwordEncoder;//Avoid use infield auto-wire annotation
      private final UserRepository userRepository;
+     private final VerificationTokenRepository verificationTokenRepository;
+     private final MailService mailService;
 
      @Transactional
      public void signup(RegisterRequest registerRequest){
@@ -28,10 +35,22 @@ public class AuthService {
           user.setEnabled(false);
           userRepository.save(user);
 
-          generateVerificationToken(user);
+          String token = generateVerificationToken(user);
+          mailService.sendMail(
+                  new NotificationEmail(
+                          "Please Activate your Account"
+                          ,user.getEmail(),
+                          "Thank you for signing up to Spring Reddit, " +
+                          "please click on the below url to activate your account : " +
+                          "http://localhost:8080/api/auth/accountVerification/" + token));
      }
-     private void generateVerificationToken(User user){
-          String verificationToken = UUID.randomUUID().toString();
+     private String generateVerificationToken(User user){
+          String token = UUID.randomUUID().toString();
+          VerificationToken verificationToken = new VerificationToken();
+          verificationToken.setToken(token);
+          verificationToken.setUser(user);
 
+          verificationTokenRepository.save(verificationToken);
+          return token;
      }
 }
