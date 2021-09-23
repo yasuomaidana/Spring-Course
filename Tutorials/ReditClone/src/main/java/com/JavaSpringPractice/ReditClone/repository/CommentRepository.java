dcbf8b43162c1ae00741fba76fdbb8f18bd17291
yasuomaidana <yasuomaidana@gmail.com>
2021-09-22 19:25:41 -0500

    Comment repository. I commented two methods since they were giving me troubles

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/repository/CommentRepository.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/repository/CommentRepository.java
@@ -1,2 +1,15 @@
-package com.JavaSpringPractice.ReditClone.repository;public class CommentRepository {
+package com.JavaSpringPractice.ReditClone.repository;
+
+import com.JavaSpringPractice.ReditClone.model.Comment;
+import com.JavaSpringPractice.ReditClone.model.Post;
+import com.JavaSpringPractice.ReditClone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    //List<Comment> findByPost(Post post);
+    //List<Comment> findAllByUser(User user);
 }
