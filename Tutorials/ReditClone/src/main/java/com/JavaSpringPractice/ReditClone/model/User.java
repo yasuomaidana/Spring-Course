457e5fa7aeaba289ad7f91c4b114e1c1cdd6677c
yasuomaidana <yasuomaidana@gmail.com>
2021-09-24 15:26:47 -0500

    User model modified to make email column unique

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/model/User.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/model/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
@@ -22,7 +19,8 @@ public class User {
     private String username;
     @NotBlank(message = "Password is required")
     private String password;
-    @Email @NotBlank(message = "Email is required")
+    @Email @Column(unique = true)
+    @NotBlank(message = "Email is required")
     private String email;
     private Instant created;
     private boolean enabled;
