e310e6a1f6cb8e2656d1a16cc7e8052ebb3adaf5
yasuomaidana <yasuomaidana@gmail.com>
2021-09-24 10:25:47 -0500

    adding variables from file

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/ReditCloneApplication.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/ReditCloneApplication.java
@@ -3,14 +3,39 @@ package com.JavaSpringPractice.ReditClone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ReditCloneApplication {
 
 	public static void main(String[] args) {
 		//Setting application.properties
 		// using https://www.logicbig.com/tutorials/spring-framework/spring-boot/different-ways-to-pass-application-properties.html
-		System.setProperty("spring.datasource.url","jdbc:mysql://localhost:3306/reddit");
+		ReditCloneApplication.setEnvironment();
+		/* You can see your environmental variables using the following lines
+		System.out.println(System.getProperty("spring.datasource.url"));
+		System.getenv().forEach((key,val)-> System.out.println(key+val));
+		*/
 		SpringApplication.run(ReditCloneApplication.class, args);
 	}
-
+	private static void setEnvironment(){
+		File envFile = new File("src/main/resources/.env");
+		try {
+			Scanner envReader = new Scanner(envFile);
+			System.out.println("Variables loaded from .env");
+			while(envReader.hasNext()){
+				String[] variable = envReader.nextLine().split("=");
+				System.setProperty(variable[0],variable.length==1? "":variable[1]);
+				System.out.println(variable[0]+"="+System.getProperty(variable[0]));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(".env file was not founded. " +
+					"\nThe variables must be defined from: " +
+					"\napplication.properties" +
+					"\n\tenvironmental variables");
+		}
+	}
 }
