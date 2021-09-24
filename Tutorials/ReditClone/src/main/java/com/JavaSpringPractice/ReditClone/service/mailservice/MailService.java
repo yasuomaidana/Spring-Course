a1b18b9fc7fc658778e7f814b329356a088a85fa
yasuomaidana <yasuomaidana@gmail.com>
2021-09-24 15:37:07 -0500

    Here you can see how we activated Async functions using notations

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/mailservice/MailService.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/mailservice/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class MailService {
     private final JavaMailSender mailSender;
     private final MailContentBuilder mailContentBuilder;
+    @Async
     public void sendMail(NotificationEmail notificationEmail){
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
