501aced012a0e2739bc62b28e2e4b17399fa3c45
YMAIDANA <ymaidana@ENCORAs-MacBook-Pro.local>
2021-09-28 15:17:50 -0500

    DTO created to receive and respond login queries

---
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/dto/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.JavaSpringPractice.ReditClone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class AuthenticationResponse  {
+    private String authenticationToken;
+    private String username;
+}
