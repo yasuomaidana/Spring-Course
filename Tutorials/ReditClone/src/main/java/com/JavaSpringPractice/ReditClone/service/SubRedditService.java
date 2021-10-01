53ab71a54c2397f2f2839755c28f726efe077752
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-09-30 19:06:47 -0500

    Long save(SubRedditDto) Method
    Here we can see how we first map the requested subreddit to a subreddit model, then it is saved. After that we set the ID to the request DTO and send it back

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/SubRedditService.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/SubRedditService.java
@@ -1,2 +1,27 @@
-package com.JavaSpringPractice.ReditClone.service;public class SubRedditService {
+package com.JavaSpringPractice.ReditClone.service;
+
+import com.JavaSpringPractice.ReditClone.dto.SubRedditDto;
+import com.JavaSpringPractice.ReditClone.model.SubReddit;
+import com.JavaSpringPractice.ReditClone.repository.SubRedditRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor @Slf4j
+public class SubRedditService {
+    private final SubRedditRepository subRedditRepository;
+
+    public SubRedditDto save(SubRedditDto subRedditDto){
+        SubReddit subReddit = mapSubRedditDto(subRedditDto);
+        SubReddit saved = subRedditRepository.save(subReddit);
+        subRedditDto.setId(saved.getId());
+        return subRedditDto;
+    }
+
+    private SubReddit mapSubRedditDto(SubRedditDto subRedditDto) {
+        return SubReddit.builder().name(subRedditDto.getSubRedditName())
+                .description(subRedditDto.getDescription())
+                .build();
+    }
 }
