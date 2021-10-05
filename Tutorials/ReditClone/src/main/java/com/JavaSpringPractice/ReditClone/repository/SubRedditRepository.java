3e0557534d170b4c2389159ce83c6bbb77cb26b4
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-05 11:37:37 -0500

    SubRedditRepository JPA created

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/repository/SubRedditRepository.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/repository/SubRedditRepository.java
@@ -1,2 +1,9 @@
-package com.JavaSpringPractice.ReditClone.repository;public interface SubRedditRepository {
+package com.JavaSpringPractice.ReditClone.repository;
+
+import com.JavaSpringPractice.ReditClone.model.SubReddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubRedditRepository extends JpaRepository<SubReddit,Long> {
 }
