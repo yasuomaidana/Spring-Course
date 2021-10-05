f371bcba689515b42b351472bd1c0c9d13a4f8e8
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-05 11:36:07 -0500

    SubredditDto created

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/dto/SubRedditDto.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/dto/SubRedditDto.java
@@ -1,2 +1,14 @@
-package com.JavaSpringPractice.ReditClone.dto;public class SubRedditDto {
+package com.JavaSpringPractice.ReditClone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class SubRedditDto {
+    private Long id;
+    private String subRedditName;
+    private String description;
+    private Integer numberOfPosts;
 }
