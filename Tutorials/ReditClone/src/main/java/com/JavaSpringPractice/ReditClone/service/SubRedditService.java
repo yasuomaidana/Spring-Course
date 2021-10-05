1568af4d9e447346393d0cdf4d8fc2d41b798ada
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-05 11:59:47 -0500

    Subreddit save and getAll methods added

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/SubRedditService.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/service/SubRedditService.java
@@ -6,17 +6,37 @@ import com.JavaSpringPractice.ReditClone.repository.SubRedditRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 
 @Service
 @AllArgsConstructor @Slf4j
 public class SubRedditService {
     private final SubRedditRepository subRedditRepository;
 
+    @Transactional
     public SubRedditDto save(SubRedditDto subRedditDto){
-        SubReddit subReddit = mapSubRedditDto(subRedditDto);
-        SubReddit saved = subRedditRepository.save(subReddit);
+        SubReddit saved = subRedditRepository.save(mapSubRedditDto(subRedditDto));
         subRedditDto.setId(saved.getId());
         return subRedditDto;
+    }
+
+    @Transactional(readOnly=true)
+    public List<SubRedditDto> getAll() {
+        return subRedditRepository.findAll().stream()
+                .map(this::mapToDto).collect(toList());
+    }
+
+    private SubRedditDto mapToDto(SubReddit subReddit) {
+        return SubRedditDto.builder().subRedditName(subReddit.getName())
+                .id(subReddit.getId())
+                .numberOfPosts(subReddit.getPosts().size())
+                .description(subReddit.getDescription())
+                .build();
     }
 
     private SubReddit mapSubRedditDto(SubRedditDto subRedditDto) {
@@ -24,4 +44,5 @@ public class SubRedditService {
                 .description(subRedditDto.getDescription())
                 .build();
     }
+
 }
