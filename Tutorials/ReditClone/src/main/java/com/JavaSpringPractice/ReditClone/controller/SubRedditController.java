046250f23b1839ad8fa3832ed48be708798177f6
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-05 11:35:14 -0500

    subreddit controller get and post paths implemented

--- Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/controller/SubRedditController.java
+++ Tutorials/ReditClone/src/main/java/com/JavaSpringPractice/ReditClone/controller/SubRedditController.java
@@ -1,2 +1,29 @@
-package com.JavaSpringPractice.ReditClone.controller;public class SubRedditController {
+package com.JavaSpringPractice.ReditClone.controller;
+
+import com.JavaSpringPractice.ReditClone.dto.SubRedditDto;
+import com.JavaSpringPractice.ReditClone.service.SubRedditService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subreddit")
+@AllArgsConstructor
+@Slf4j
+public class SubRedditController {
+    private final SubRedditService subRedditService;
+    @PostMapping
+    public ResponseEntity<SubRedditDto> createSubReddit(@RequestBody SubRedditDto subRedditDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(subRedditService.save(subRedditDto));
+    }
+    @GetMapping
+    public ResponseEntity<List<SubRedditDto>> getAllSubReddit(){
+         return ResponseEntity.status(HttpStatus.OK)
+                 .body(subRedditService.getAll());
+    }
 }
