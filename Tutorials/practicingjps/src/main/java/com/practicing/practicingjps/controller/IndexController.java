d39c79e493ea680f912f513e4751cf421c540d18
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-07 16:42:58 -0500

    index controller implemented

--- Tutorials/practicingjps/src/main/java/com/practicing/practicingjps/controller/IndexController.java
+++ Tutorials/practicingjps/src/main/java/com/practicing/practicingjps/controller/IndexController.java
@@ -1,2 +1,12 @@
-package com.practicing.practicingjps.controller;public class IndexController {
+package com.practicing.practicingjps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
