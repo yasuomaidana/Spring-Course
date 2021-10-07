0fb3c00576fb3c57b79c4c8b8f4b7a2fda8de1ae
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-06 21:47:07 -0500

    JPS practice started

---
+++ Tutorials/practicing_jps/src/test/java/practice/practicing_jps/PracticingJpsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.practicing_jps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingJpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
