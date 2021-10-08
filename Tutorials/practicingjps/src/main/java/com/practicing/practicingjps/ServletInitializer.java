4216d458e6297aa091dc21367bdf5bcdb7ec1d12
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-08 14:35:01 -0500

    Descriptions added

--- Tutorials/practicingjps/src/main/java/com/practicing/practicingjps/ServletInitializer.java
+++ Tutorials/practicingjps/src/main/java/com/practicing/practicingjps/ServletInitializer.java
@@ -3,7 +3,7 @@ package com.practicing.practicingjps;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class  ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
