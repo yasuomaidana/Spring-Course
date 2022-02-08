c943a9f947e624a04c23d56737c452c38f3d761d
Yasuo <yasuomaidana@gmail.com>
2022-02-07 18:08:52 -0600

    removing unused imports

--- Tutorials/redditclone/reddit_backend/src/main/java/com/JavaSpringPractice/ReditClone/config/SwaggerConfiguration.java
+++ Tutorials/redditclone/reddit_backend/src/main/java/com/JavaSpringPractice/ReditClone/config/SwaggerConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
