af6b1e6ff8cd491a1c70c0db791d017f8fd8f86e
Yasuo <yasuomaidana@gmail.com>
2022-03-01 12:36:31 -0600

    adding getAllSubreddit and subreddit model

---
+++ Tutorials/redditclone/reddit-frontend/src/app/shared/models/subreddit-model.ts
@@ -0,0 +1,6 @@
+export class subredditModel{
+  id?:number;
+  name:string;
+  description:string;
+  postcount?:number;
+}
--- Tutorials/redditclone/reddit-frontend/src/app/shared/subreddit/subreddit.service.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/shared/subreddit/subreddit.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { subredditModel } from '../models/subreddit-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubredditService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllSubreddit():Observable<Array<subredditModel>>{
+    return this.http
+    .get<Array<subredditModel>>(environment.backendHost+"/subreddit");
+  }
 }
