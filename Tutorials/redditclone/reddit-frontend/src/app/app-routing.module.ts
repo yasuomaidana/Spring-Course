27f5ee20eaa7aa69a5f759dc897ab5761c6f6252
Yasuo <yasuomaidana@gmail.com>
2022-03-01 16:15:30 -0600

    adding new paths

--- Tutorials/redditclone/reddit-frontend/src/app/app-routing.module.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'signup', component:SignupComponent },
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'create-post',component:CreatePostComponent},
+  {path:'create-subreddit',component:CreateSubredditComponent}
 ];
 
 @NgModule({
