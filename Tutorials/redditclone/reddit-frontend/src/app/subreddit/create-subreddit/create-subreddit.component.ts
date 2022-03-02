b2bd512142dc4b565d0530275db282acb057e829
Yasuo <yasuomaidana@gmail.com>
2022-03-02 15:34:49 -0600

    adding creating logic

--- Tutorials/redditclone/reddit-frontend/src/app/subreddit/create-subreddit/create-subreddit.component.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/subreddit/create-subreddit/create-subreddit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SubredditModel } from 'src/app/shared/models/subreddit-model';
+import { SubredditService } from 'src/app/shared/subreddit/subreddit.service';
 
 @Component({
   selector: 'app-create-subreddit',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-subreddit.component.scss']
 })
 export class CreateSubredditComponent implements OnInit {
+  createSubredditForm: FormGroup;
+  subredditModel: SubredditModel={
+    subRedditName:"",
+    description:""
+  };
 
-  constructor() { }
+  constructor(private router: Router,
+    private subredditService: SubredditService) {
+      this.createSubredditForm = new FormGroup({
+        title: new FormControl('', Validators.required),
+        description: new FormControl('', Validators.required)
+      });
+   }
 
   ngOnInit(): void {
+  }
+
+  discard() {
+    this.router.navigateByUrl('/');
+  }
+
+  createSubreddit() {
+    this.subredditModel.subRedditName = this.createSubredditForm.get('title')?.value;;
+    this.subredditModel.description = this.createSubredditForm.get('description')?.value;
+    this.subredditService.createSubreddit(this.subredditModel).subscribe(data => {
+      this.router.navigateByUrl('/');
+    }, (error) => {
+      console.log('Error occurred');
+    })
   }
 
 }
