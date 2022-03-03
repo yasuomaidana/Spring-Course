18d7983813604a333267d2379399da919782638b
Yasuo <yasuomaidana@gmail.com>
2022-03-03 13:39:25 -0600

    adding comment service to post and show

---
+++ Tutorials/redditclone/reddit-frontend/src/app/shared/comment/comment.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CommentPayload } from '../payloads/comment.payload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  constructor(private http:HttpClient) { }
+
+  getAllCommentsForPost(postId: number):Observable<CommentPayload[]> {
+    return this.http.get<CommentPayload[]>(environment.backendHost+'/comments/by-post/' + postId);
+  }
+
+  postComment(commentPayload: CommentPayload):Observable<any> {
+    return this.http.post<any>(environment.backendHost+'/comments/', commentPayload);
+  }
+}
---
+++ Tutorials/redditclone/reddit-frontend/src/app/shared/payloads/comment.payload.ts
@@ -0,0 +1,6 @@
+export class CommentPayload{
+  text: string;
+  postId: string;
+  username?:string;
+  duration?: string;
+}
