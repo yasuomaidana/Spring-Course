bdf7566e2e8bf5802a442a2e394ec16dec3755e5
Yasuo <yasuomaidana@gmail.com>
2022-03-03 13:40:07 -0600

    modifying viewpos page to show and modify comments

--- Tutorials/redditclone/reddit-frontend/src/app/post/view-post/view-post.component.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/post/view-post/view-post.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { throwError } from 'rxjs';
+import { CommentService } from 'src/app/shared/comment/comment.service';
 import { PostModel } from 'src/app/shared/models/post-model';
+import { CommentPayload } from 'src/app/shared/payloads/comment.payload';
 import { PostService } from 'src/app/shared/post/post.service';
 
 @Component({
@@ -12,21 +15,57 @@ import { PostService } from 'src/app/shared/post/post.service';
 export class ViewPostComponent implements OnInit {
 
   postId: number;
-  post$: PostModel={
+  post: PostModel={
     id: 0,
     postName: '',
     description: '',
     subRedditName: ''
   };
-  constructor(private postService: PostService, private activateRoute: ActivatedRoute) {}
+  commentForm: FormGroup;
+  commentPayload: CommentPayload;
+  comments: CommentPayload[];
+  constructor(private postService: PostService,
+    private activateRoute: ActivatedRoute,
+    private commentService: CommentService,
+    private router: Router) {
+    this.postId = this.activateRoute.snapshot.params.id;
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required)
+    });
+    this.commentPayload = {
+      text: '',
+      postId: this.postId.toString()
+    };
+  }
 
   ngOnInit(): void {
-    this.postId = this.activateRoute.snapshot.params.id;
+    this.getPostById();
+    this.getCommentsForPost();
+  }
+
+  postComment() {
+    this.commentPayload.text = this.commentForm.get('text')?.value;
+    this.commentService.postComment(this.commentPayload).subscribe(data => {
+      this.commentForm.get('text')?.setValue('');
+      this.getCommentsForPost();
+    }, error => {
+      throwError(error);
+    })
+  }
+
+  private getPostById() {
     this.postService.getPost(this.postId).subscribe(data => {
-      this.post$ = data;
+      this.post = data;
     }, error => {
       throwError(error);
     });
   }
 
+  private getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.postId).subscribe(data => {
+      this.comments = data;
+    }, error => {
+      throwError(error);
+    });
+  }
 }
