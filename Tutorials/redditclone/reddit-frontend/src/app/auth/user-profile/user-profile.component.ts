04b219de8ff6e87f487ac8fcb4b164cb3df0f02e
Yasuo <yasuomaidana@gmail.com>
2022-03-05 15:40:12 -0600

    adding logic to user profile view

--- Tutorials/redditclone/reddit-frontend/src/app/auth/user-profile/user-profile.component.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/auth/user-profile/user-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommentService } from 'src/app/shared/comment/comment.service';
+import { PostModel } from 'src/app/shared/models/post-model';
+import { CommentPayload } from 'src/app/shared/payloads/comment.payload';
+import { PostService } from 'src/app/shared/post/post.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  posts: PostModel[]=[];
+  comments: CommentPayload[]=[];
+  postLength: number;
+  commentLength: number;
+
+  constructor(private activatedRoute: ActivatedRoute, private postService: PostService,
+    private commentService: CommentService) { }
 
   ngOnInit(): void {
+    this.name = this.activatedRoute.snapshot.params.name;
+
+    this.postService.getAllPostsByUser(this.name).subscribe(data => {
+      this.posts = data;
+      this.postLength = data.length;
+    });
+    this.commentService.getAllCommentsByUser(this.name).subscribe(data => {
+      this.comments = data;
+      this.commentLength = data.length;
+    });
   }
 
 }
