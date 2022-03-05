215e758a31c7501346fa1a7f04a75fd1722641b3
Yasuo <yasuomaidana@gmail.com>
2022-03-04 21:19:55 -0600

    modifying view to route to user profile info

--- Tutorials/redditclone/reddit-frontend/src/app/header/header.component.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn:boolean=false;
+  username:string;
+
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
   }
 
+  goToUserProfile(){}
+  logout(){}
 }
