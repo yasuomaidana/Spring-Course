cfb733ccb27a1885fb80b1c494a678dd3f9a5cb9
Yasuo <yasuomaidana@gmail.com>
2022-03-04 21:18:58 -0600

    adding user profile view

--- Tutorials/redditclone/reddit-frontend/src/app/app-routing.module.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 import { HomeComponent } from './home/home/home.component';
 import { CreatePostComponent } from './post/create-post/create-post.component';
 import { ViewPostComponent } from './post/view-post/view-post.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'signup', component:SignupComponent },
   {path:'login',component:LoginComponent},
+  {path:'user-profile',component:UserProfileComponent},
   {path:'create-post',component:CreatePostComponent},
   {path:'create-subreddit',component:CreateSubredditComponent},
   {path:'list-subreddits',component:ListSubredditsComponent},
--- Tutorials/redditclone/reddit-frontend/src/app/app.module.ts
+++ Tutorials/redditclone/reddit-frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { EditorModule } from '@tinymce/tinymce-angular';
 import { ViewPostComponent } from './post/view-post/view-post.component';
 import { GuidelinesComponent } from './guidelines/guidelines.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     CreatePostComponent,
     ListSubredditsComponent,
     ViewPostComponent,
-    GuidelinesComponent
+    GuidelinesComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
