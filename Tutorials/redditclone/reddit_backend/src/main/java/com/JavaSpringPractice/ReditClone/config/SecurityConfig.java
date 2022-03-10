ddd493296363329f59482f248b276d47b8724d33
Yasuo <yasuomaidana@gmail.com>
2022-03-10 17:48:14 -0600

    adding post/** as secured path

--- Tutorials/redditclone/reddit_backend/src/main/java/com/JavaSpringPractice/ReditClone/config/SecurityConfig.java
+++ Tutorials/redditclone/reddit_backend/src/main/java/com/JavaSpringPractice/ReditClone/config/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET,"/api/posts")
                 .permitAll()
+                .antMatchers(HttpMethod.GET,"/api/posts/**")
+                .permitAll()
                 .antMatchers("/v2/api-docs",
                         "/configuration/ui",
                         "/swagger-resources/**",
